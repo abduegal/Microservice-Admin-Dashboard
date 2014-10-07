7e5be43c4186a1c223c83451a5a9ce3369500182
Felix Braun <felix.braun@acrolinx.com>
2014-10-07 17:23:50 +0200

    small refactorings in GraphDataGenerator
    * removed catch for unthrown exception
    * added generic type information
    * add log statement in case of error when graph is build

--- dashboard/src/main/java/com/aegal/frontend/srv/GraphDataGenerator.java
+++ dashboard/src/main/java/com/aegal/frontend/srv/GraphDataGenerator.java
@@ -2,16 +2,20 @@ package com.aegal.frontend.srv;
 
 import com.aegal.framework.core.ServiceLocator;
 import com.aegal.framework.core.api.Connections;
-import com.aegal.framework.core.exceptions.ServiceCallException;
 import com.aegal.framework.core.util.Guard;
 import com.aegal.frontend.DependencyKeys;
 import com.aegal.frontend.dto.D3GraphDTO;
 import com.ge.snowizard.discovery.core.InstanceMetadata;
 import com.yammer.tenacity.core.TenacityCommand;
+
 import feign.FeignException;
+
 import org.apache.curator.x.discovery.ServiceInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.WebApplicationException;
+
 import java.util.*;
 
 /**
@@ -21,6 +25,9 @@ import java.util.*;
  */
 public class GraphDataGenerator extends TenacityCommand<List<D3GraphDTO<InstanceMetadata>>> {
 
+    private static final Logger logger = LoggerFactory
+	    .getLogger(GraphDataGenerator.class);
+    
     private final ServiceLocator serviceLocator;
     private static Map<String, Integer> serviceNameGroupId = new HashMap<>();
 
@@ -50,7 +57,7 @@ public class GraphDataGenerator extends TenacityCommand<List<D3GraphDTO<Instance
 
     }
 
-    private void buildGraphDto(D3GraphDTO graphDTO, ServiceInstance<InstanceMetadata> instance, String servicename,
+    private void buildGraphDto(D3GraphDTO<InstanceMetadata> graphDTO, ServiceInstance<InstanceMetadata> instance, String servicename,
                                Map<String, Collection<ServiceInstance<InstanceMetadata>>> instances) {
 
         graphDTO.id = instance.getPayload().getInstanceId().toString();
@@ -66,12 +73,13 @@ public class GraphDataGenerator extends TenacityCommand<List<D3GraphDTO<Instance
                     serviceLocator.build(instance.getPayload(), Connections.class).getConnections();
             graphDTO.links = instanceMetadatas;
 
-        } catch (ServiceCallException | WebApplicationException | FeignException e) {
+        } catch (WebApplicationException | FeignException e) {
+            logger.error("Error while building the d3 graph: " + e.getMessage());
         }
 
     }
 
-    private int groupForServiceName(String serviceName) {
+    private static int groupForServiceName(String serviceName) {
         Integer id = serviceNameGroupId.get(serviceName);
         if (id == null) {
             id = serviceNameGroupId.size();
